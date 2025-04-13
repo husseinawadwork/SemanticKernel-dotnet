3eba24463ce34f8281fa2935c57d6e9675f4f0d6
Hussein <hussein.awad.work@outlook.com>
2025-04-13 16:43:23 +1000

    Add initial project files for HelloAIChatApp including .gitignore, project file, and main program

---
+++ 01 - HelloAIChatApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+
+
+IChatClient client = new ChatCompletionsClient(
+        endpoint: new Uri("%MODEL INFERENCE ENDPOINT%"),
+        new AzureKeyCredential("AZURE_OPENAI_API_KEY") ??
+         throw new InvalidOperationException("Missing API Key."))
+        .AsChatClient("DEPLOYMENT_NAME");   
+
+//IChatClient client = new OllamaChatClient(new Uri("http://localhost:11434/"), "phi4-mini");
+
+var response = await client.GetResponseAsync("What is AI?");
+
+Console.WriteLine(response.Text);
