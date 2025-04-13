cb4acd94b3922db9743c4e5b8ac4b9a027760ab1
Hussein <hussein.awad.work@outlook.com>
2025-04-13 17:11:48 +1000

    Update placeholders for Azure OpenAI API key and deployment name in Program.cs

--- 01 - HelloAIChatApp/Program.cs
+++ 01 - HelloAIChatApp/Program.cs
@@ -5,9 +5,9 @@ using Microsoft.Extensions.AI;
 
 IChatClient client = new ChatCompletionsClient(
         endpoint: new Uri("%MODEL INFERENCE ENDPOINT%"),
-        new AzureKeyCredential("AZURE_OPENAI_API_KEY") ??
+        new AzureKeyCredential("%AZURE_OPENAI_API_KEY%") ??
          throw new InvalidOperationException("Missing API Key."))
-        .AsChatClient("DEPLOYMENT_NAME");   
+        .AsChatClient("%DEPLOYMENT_NAME%");   
 
 //IChatClient client = new OllamaChatClient(new Uri("http://localhost:11434/"), "phi4-mini");
 
