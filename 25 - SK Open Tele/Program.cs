8731a2b5c34af1e74c0ecda1da771a292b33d303
Hussein <hussein.awad.work@outlook.com>
2025-04-20 15:10:19 +1000

    Add "25 - SK Open Tele" project with initial implementation, including telemetry setup and OpenTelemetry integration

---
+++ 25 - SK Open Tele/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace TelemetryConsoleQuickstart
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Telemetry setup code goes here
+            var resourceBuilder = ResourceBuilder
+                .CreateDefault()
+                .AddService("TelemetryConsoleQuickstart");
+
+            // Enable model diagnostics with sensitive data.
+            AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+
+using var traceProvider = Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(resourceBuilder)
+    .AddSource("Microsoft.SemanticKernel*")
+    .AddConsoleExporter()
+    .Build();
+
+using var meterProvider = Sdk.CreateMeterProviderBuilder()
+    .SetResourceBuilder(resourceBuilder)
+    .AddMeter("Microsoft.SemanticKernel*")
+    .AddConsoleExporter()
+    .Build();
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    // Add OpenTelemetry as a logging provider
+    builder.AddOpenTelemetry(options =>
+    {
+        options.SetResourceBuilder(resourceBuilder);
+        options.AddConsoleExporter();
+        // Format log messages. This is default to false.
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+    });
+    builder.SetMinimumLevel(LogLevel.Trace);
+});
+
+            IKernelBuilder builder = Kernel.CreateBuilder();
+             builder.Services.AddSingleton(loggerFactory);
+            builder.AddAzureOpenAIChatCompletion(
+                "%DEPLOYMENT_NAME%",
+                "%AZURE_OPENAI_ENDPOINT%",
+                "%AZURE_OPENAI_API_KEY%"
+            );
+
+            Kernel kernel = builder.Build();
+
+            var answer = await kernel.InvokePromptAsync(
+                "Why is the sky blue in one sentence?"
+            );
+
+            Console.WriteLine(answer);
+        }
+    }
+}
