a57f6cdf523c43986d382d9fd012c555fb4f5c63
Hussein <hussein.awad.work@outlook.com>
2025-04-24 05:58:29 +1000

    Add "46 - ChatLoop with Prompt" project with initial implementation, including chat setup, Azure OpenAI integration, and user message handling

---
+++ 46 - ChatLoop with Prompt/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+
+var kernel = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion
+    ("%DEPLOYMENT_NAME%",
+            "%AZURE_OPENAI_ENDPOINT%",
+            "%AZURE_OPENAI_APIKEY%").Build();
+
+
+var chatHistory = new ChatHistory();
+KernelArguments arguments = new() { { "chatHistory", chatHistory } };
+string[] userMessages = [
+           "What is Seattle?",
+            "What is the population of Seattle?",
+            "What is the area of Seattle?",
+            "What is the weather in Seattle?",
+            "What is the zip code of Seattle?",
+            "What is the elevation of Seattle?",
+            "What is the latitude of Seattle?",
+            "What is the longitude of Seattle?",
+            "What is the mayor of Seattle?"
+       ];
+
+
+foreach (var userMessage in userMessages)
+{
+    chatHistory.AddUserMessage(userMessage);
+    Console.WriteLine(chatHistory.Last().Role + ": " + chatHistory.Last().Content);
+
+    var function = kernel.CreateFunctionFromPrompt(
+        new()
+        {
+            Template =
+            """
+                    {{#each (chatHistory)}}
+                    <message role="{{Role}}">{{Content}}</message>
+                    {{/each}}
+                    """,
+            TemplateFormat = "handlebars"
+        },
+        new HandlebarsPromptTemplateFactory()
+    );
+
+    var response = await kernel.InvokeAsync(function, arguments);
+
+    chatHistory.AddAssistantMessage(response.ToString());
+    Console.WriteLine(chatHistory.Last().Role + ": " + chatHistory.Last().Content);
+}
